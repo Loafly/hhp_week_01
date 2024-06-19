64a41fcfb02931f7bee82aefc98fd04e24f9aaf7
namseokkim <skatjr30@gmail.com>
2024-06-19 21:12:28 +0900

    test : 포인트 사용 api 테스트 케이스 작성 - 포인트 정상 사용 - 포인트 사용금액이 0원 미만일 경우 - 포인트 사용금액이 가지고 있는 포인트보다 큰 경우

--- src/test/kotlin/io/hhplus/tdd/point/integration/PointControllerApiTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/integration/PointControllerApiTest.kt
@@ -98,7 +98,54 @@ class PointControllerApiTest {
         }
     }
 
-    @Test
-    fun use() {
+    @Nested
+    @DisplayName("포인트 사용")
+    inner class PointUseTest {
+        @Test
+        fun `포인트 정상 사용`() {
+            val id: Long = 0
+            val amount: Long = 0
+            val requestBody = objectMapper.writeValueAsString(amount)
+
+            // when then
+            mockMvc.perform(
+                MockMvcRequestBuilders.patch("/point/$id/use")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody)
+            )
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.point").value(amount))
+        }
+
+        @Test
+        fun `포인트 사용 금액 이 0원 미만일 경우`() {
+            val id: Long = 0
+            val amount: Long = -1000
+            val requestBody = objectMapper.writeValueAsString(amount)
+
+            // when then
+            mockMvc.perform(
+                MockMvcRequestBuilders.patch("/point/$id/use")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody)
+            )
+                .andExpect(status().is4xxClientError)
+        }
+
+        @Test
+        fun `포인트 사용 금액이 가지고 있는 포인트보다 큰 경우`() {
+            val id: Long = 0
+            val amount: Long = 1000
+            val requestBody = objectMapper.writeValueAsString(amount)
+
+            // when then
+            mockMvc.perform(
+                MockMvcRequestBuilders.patch("/point/$id/use")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody)
+            )
+                .andExpect(status().is4xxClientError)
+        }
     }
 }
